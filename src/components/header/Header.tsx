578342a79c00fa9791df4ec758aa8f9da7bfbe70
JoaquinStechina <joaquinstechina12@gmail.com>
2025-03-30 02:32:09 -0300

    fix: fixed dashed border at the sides of header and footer

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,7 +5,7 @@ import Linkedin from "../ui/Linkedin";
 import Portfolio from "../ui/Portfolio";
 const Header: React.FC = () => {
   return (
-    <header className="sticky pl-1 pr-5 pt-2 pb-2 grid grid-cols-2 border border-t-0 border-2 border-dashed flex items-center">
+    <header className="sticky pl-1 pr-5 pt-2 pb-2 grid grid-cols-2 border border-t-0 border-l-0 border-r-0 border-2 border-dashed flex items-center">
       <nav>
         <h1 className="text-xl">Compound Interest Calculator</h1>
       </nav>
